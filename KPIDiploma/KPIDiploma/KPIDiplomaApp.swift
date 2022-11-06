95b96b02f4584d053e2b32e26e609b632fd2bb79
Fedor <kavunfedor01@gmail.com>
2022-11-06 19:46:57 +0200

    Create project

---
+++ KPIDiploma/KPIDiploma/KPIDiplomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPIDiplomaApp.swift
+//  KPIDiploma
+//
+//  Created by Fyodor Kavun on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct KPIDiplomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
